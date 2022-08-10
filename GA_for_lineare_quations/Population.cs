ac54ec4c57606ce7ac5b87e043b64ebd946ed471
Kieran200 <ann.funtikova06@mail.ru>
2022-08-10 12:13:10 +0300

    1

---
+++ GA_for_lineare_quations/Population.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GA_for_lineare_quations
+{
+    class Population
+    {
+        public int x;
+        public int survival_rate;
+        public float survival_percent;
+        public Population(int number, int rate)
+        {
+            x = number;
+            survival_rate = rate;
+        }
+    }
+}
